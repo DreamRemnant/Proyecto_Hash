049191861a0f9407929a878da06e48e3c61d4824
DreamRemnant <jfmg.64@gmail.com>
2023-06-27 20:06:03 -0700

    Agregar archivos de proyecto.

---
+++ Proyecto_Hash/ListaLigada.cs
@@ -0,0 +1,26 @@
+﻿namespace Proyecto_Hash
+{
+    internal class ListaLigada
+    {
+        Nodo nodoInicial;
+        public ListaLigada()
+        {
+            nodoInicial = new Nodo();
+        }
+        public void Agregar(string valor)
+        {
+            Nodo nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+            }
+
+            nodoActual.Siguiente = new Nodo(valor);
+        }
+
+        public void Relacionar(ListaLigada vertice)
+        {
+            Agregar(vertice.nodoInicial.Valor);
+        }
+    }
+}
---
+++ Proyecto_Hash/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Hash
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] palabras = new string[3];
+            ListaLigada[] tablaHash = new ListaLigada[7];
+
+            /*
+            palabras[0] = "Hol";
+            palabras[2] = "Adi";
+
+            Console.WriteLine(palabras[0]);
+            */
+        }
+    }
+}
